c409d7fb657fe5c3433c41114ddab3d8a59322fd
onsar <oscar.puyal@gmail.com>
2018-08-20 12:34:40 +0200

    añadir registros del 15 al 24

--- src/modbus_cvm1d.h
+++ src/modbus_cvm1d.h
@@ -19,12 +19,13 @@ PROGRAM: [=         ]  10.3% (used 432488 bytes from 4194304 bytes)
 
 #define NUMBER_OF_REGISTERS 24
 
+#define REGISTERS_TO_READ 15
 
 #define PARAMETER_LIST "V_1","A_1","Kw_1","Kvar_1","PF_1",\
-                        "V_2","A_2","Kw_2","Kvar_2","PF_2",\
-                        "V_3","A_3","Kw_3","Kvar_3","PF_3",\
-                        "Kw_III","KvarL_III","KvarC_III","Cos_III","PFIII",\
-                        "Hz","V12","V23","V31"
+                       "V_2","A_2","Kw_2","Kvar_2","PF_2",\
+                       "V_3","A_3","Kw_3","Kvar_3","PF_3",\
+                       "Kw_III","KvarL_III","KvarC_III","Cos_III","PFIII",\
+                       "Hz","V12","V23","V31"
 
 #define TRANSMISSION_LIST "Kw_1","Kvar_1",\
                           "Kw_2","Kvar_2",\
@@ -89,18 +90,18 @@ long two_register_to_long(uint16_t high_16, uint16_t low_16){
   return both_long;
 }
 
-void result_to_register(int n){
+void result_to_register(int n, int first){
   Serial.println("_result_to_tx_register_");
 
   long both_long = 0;
   both_long = two_register_to_long(node.getResponseBuffer(n*2),node.getResponseBuffer((n*2)+1));
-  tx_values[n] = (float)both_long/(float)factor_list[n];
-  Serial.println(n);
+  tx_values[n+first] = (float)both_long/(float)factor_list[n+first];
 
-  Serial.print("both_long --> ");
-  Serial.println(both_long);
-  Serial.print("media que se almacena para tx: ");
-  Serial.println(tx_values[n]);
+  Serial.print(n+first);
+  Serial.print("  both_long: ");
+  Serial.print(both_long);
+  Serial.print(" - float: ");
+  Serial.println(tx_values[n+first]);
 }
 
 
@@ -200,9 +201,12 @@ void modbus_setup()
   Serial.println("_writeMultipleRegisters_044C_");
   node.writeMultipleRegisters(0x044C, 6);
 
+  Serial.println("");
   Serial.println("_readInputRegisters_044C_");
+  delay(10000);
   uint8_t result;
   result = node.readInputRegisters(0x044C, 6);
+  Serial.print("result = ");
   Serial.println(result);
 
   if (result == node.ku8MBSuccess)
@@ -264,94 +268,52 @@ void modbus_loop()
 
 
 
-  Serial.println("_readInputRegisters_");
+  Serial.println("_readInputRegisters_1_");
   delay(10000);  //Necesario para darle tiempo al equipo de CIRCUTOR
   uint8_t result;
-  result = node.readInputRegisters(0x0000, (30));
+
+  result = node.readInputRegisters(0x0000, (REGISTERS_TO_READ*2));
+  // result = node.readInputRegisters(0x001E, (18));
   Serial.println(result);
 
   if (result == node.ku8MBSuccess)
   {
 
-    for (int i =0; i < (15-1); i++) {
-      result_to_register(i);
+    for (int i =0; i < (REGISTERS_TO_READ); i++) {
+      result_to_register(i,0);
     }
 
-    for (int i = 0; i < (15-1); i++) {
+    for (int i = 0; i < (REGISTERS_TO_READ); i++) {
       Serial.print(array_parameters[i]);
       Serial.print(" ---> ");
       Serial.println(tx_values[i]);
     }
-
-    Serial.print("0x00:Vr: ");
-    Serial.println(node.getResponseBuffer(0x00),HEX);
-    Serial.print("0x01:Vr: ");
-    Serial.println(node.getResponseBuffer(0x01),HEX);
-
-    Serial.print("0x02:Irms: ");
-    Serial.println(node.getResponseBuffer(0x02));
-    Serial.print("0x03:Irms: ");
-    Serial.println(node.getResponseBuffer(0x03));
-    Serial.print("0x04:Pa: ");
-    Serial.println(node.getResponseBuffer(0x04));
-    Serial.print("0x05:Pa: ");
-    Serial.println(node.getResponseBuffer(0x05));
-    Serial.print("0x06:Pr: ");
-    Serial.println(node.getResponseBuffer(0x06),HEX);
-    Serial.print("0x07:Pr: ");
-    Serial.println(node.getResponseBuffer(0x07),HEX);
-    Serial.print("0x08:Pf ");
-    Serial.println(node.getResponseBuffer(0x08),HEX);
-    Serial.print("0x09:Pf ");
-    Serial.println(node.getResponseBuffer(0x09),HEX);
-
-    Serial.print("0x0A:Vs: ");
-    Serial.println(node.getResponseBuffer(0x0A));
-    Serial.print("0x0B:Vs: ");
-    Serial.println(node.getResponseBuffer(0x0B));
-
-    Serial.print("0x0C: ");
-    Serial.println(node.getResponseBuffer(0x0C));
-    Serial.print("0x0D: ");
-    Serial.println(node.getResponseBuffer(0x0D));
-    Serial.print("0x0E: ");
-    Serial.println(node.getResponseBuffer(0x0E));
-    Serial.print("0x0F: ");
-    Serial.println(node.getResponseBuffer(0x0F));
-    Serial.print("0x10: ");
-    Serial.println(node.getResponseBuffer(0x10));
-    Serial.print("0x11: ");
-    Serial.println(node.getResponseBuffer(0x11));
-    Serial.print("0x12: ");
-    Serial.println(node.getResponseBuffer(0x12));
-    Serial.print("0x13: ");
-    Serial.println(node.getResponseBuffer(0x13));
-
-    Serial.print("0x14:Vt ");
-    Serial.println(node.getResponseBuffer(0x14));
-    Serial.print("0x15:Vt: ");
-    Serial.println(node.getResponseBuffer(0x15));
-
-    Serial.print("0x16: ");
-    Serial.println(node.getResponseBuffer(0x16));
-    Serial.print("0x17: ");
-    Serial.println(node.getResponseBuffer(0x17));
-    Serial.print("0x18: ");
-    Serial.println(node.getResponseBuffer(0x18));
-    Serial.print("0x19: ");
-    Serial.println(node.getResponseBuffer(0x19));
-    Serial.print("0x1A: ");
-    Serial.println(node.getResponseBuffer(0x1A));
-    Serial.print("0x1B: ");
-    Serial.println(node.getResponseBuffer(0x1B));
-    Serial.print("0x1C: ");
-    Serial.println(node.getResponseBuffer(0x1C));
-    Serial.print("0x1D: ");
-    Serial.println(node.getResponseBuffer(0x1D));
-
   }
   else {
-    Serial.println("la lectura de registros NO es CORRECTA");
+    Serial.println("la lectura NO es CORRECTA");
+    default_value_to_register();
+  }
+
+  Serial.println("_readInputRegisters_2_");
+  delay(10000);  //Necesario para darle tiempo al equipo de CIRCUTOR
+
+  result = node.readInputRegisters(0x001E, (18));
+    // result = node.readInputRegisters(0x001E, (18));
+  Serial.println(result);
+
+  if (result == node.ku8MBSuccess)
+  {
+    for (int i =0; i < (9); i++) {
+    result_to_register(i,15);
+    }
+    for (int i = 15; i < (24); i++) {
+      Serial.print(array_parameters[i]);
+      Serial.print(" ---> ");
+      Serial.println(tx_values[i]);
+    }
+  }
+  else {
+    Serial.println("la lectura NO es CORRECTA");
     default_value_to_register();
   }
 
